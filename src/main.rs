ef717b6c70def63386adca41f962779b2877ef8d
breakfast <qwertyytheartist@gmail.com>
2024-12-06 16:09:20 -0800

    fetching function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Url;
+
+async fn fetch_from_url(url: &str) -> reqwest::Result<String> {
+    let response_text = reqwest::get(url).await?.text().await?;
+    Ok(response_text)
+}
+
+#[tokio::main]
+async fn main() {
+    match fetch_from_url("").await {
+        Ok(response) => println!("{}", response),
+        Err(err) => eprintln!("{:?}", err)
+    }
 }
