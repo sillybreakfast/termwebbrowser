2132af869b1c6b5298e7ab00a4224e3a9160feee
breakfast <qwertyytheartist@gmail.com>
2024-12-06 16:59:37 -0800

    add defaults

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use serde_json::Value;
+
 async fn fetch_from_url(url: &str) -> reqwest::Result<String> {
     let response_text = reqwest::get(url).await?.text().await?;
     Ok(response_text)
@@ -9,7 +11,13 @@ async fn main() {
         Ok(response) => response,
         Err(err) => format!("{{ \"title\": \"error\", \"content\": \"{:?}\" }}", err).as_str().to_owned()
     };
-    let site_json: serde_json::Value = serde_json::from_str(fetched_site.as_str()).unwrap();
-    println!("\x1b[1m{}\x1b[0m", site_json["title"].as_str().unwrap());
-    println!("{}", site_json["content"].as_str().unwrap());
+    let site_json: Value = serde_json::from_str(fetched_site.as_str()).unwrap();
+    println!("\x1b[1m{}\x1b[0m", match site_json.get("title") {
+        Some(title) => title.as_str().unwrap(),
+        _ => "untitled"
+    });
+    println!("{}", match site_json.get("content") {
+        Some(content) => content.as_str().unwrap(),
+        _ => ""
+    });
 }
